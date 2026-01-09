5424680f7814ff6273368594178ded687ba546a9
Bhavishya khemnani <74811040+Bobysoni199@users.noreply.github.com>
2026-01-09 11:54:21 +0530

    committed

---
+++ Program.cs
@@ -0,0 +1,18 @@
+@page
+@model IndexModel
+@{
+    ViewData["Title"] = "Home Page";
+}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>@ViewData["Title"]</title>
+</head>
+<body>
+    <h1>Welcome to .NET 10 Simple Web Page!</h1>
+    <p>This is a minimal web page created using .NET 10 and Razor Pages.</p>
+</body>
+</html>
+
